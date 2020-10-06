3fa797dfb7d772b4e115371d85647d03dbc5c8e9
Pratikshit26 <pratikshitpfd@gmail.com>
2020-10-06 21:36:51 +0530

    added components -> date, stats , chart

---
+++ App.js
@@ -0,0 +1,140 @@
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
+import React from "react";
+import { Animated, View, StyleSheet, Text, ScrollView } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { VictoryPie } from "victory-native";
+import Date from "./src/date";
+import Chart from "./src/chart";
+import Stats from "./src/stats";
+
+import Attendance from "./src/attendance";
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
+
+export default function App() {
+  return (
+    <AnimatedAppLoader image={{ uri: Constants.manifest.splash.image }}>
+      <MainScreen />
+    </AnimatedAppLoader>
+  );
+}
+
+function AnimatedAppLoader({ children, image }) {
+  const [isSplashReady, setSplashReady] = React.useState(false);
+
+  const startAsync = React.useMemo(
+    // If you use a local image with require(...), use `Asset.fromModule`
+    () => () => Asset.fromURI(image).downloadAsync(),
+    [image]
+  );
+
+  const onFinish = React.useMemo(() => setSplashReady(true), []);
+
+  if (!isSplashReady) {
+    return (
+      <AppLoading
+        startAsync={startAsync}
+        onError={console.error}
+        onFinish={onFinish}
+      />
+    );
+  }
+
+  return <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>;
+}
+
+function AnimatedSplashScreen({ children, image }) {
+  const animation = React.useMemo(() => new Animated.Value(1), []);
+  const [isAppReady, setAppReady] = React.useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = React.useState(
+    false
+  );
+
+  React.useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = React.useMemo(() => async () => {
+    SplashScreen.hideAsync();
+    try {
+      // Load stuff
+      await Promise.all([]);
+    } catch (e) {
+      // handle errors
+    } finally {
+      setAppReady(true);
+    }
+  });
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: Constants.manifest.splash.backgroundColor,
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            style={{
+              width: "100%",
+              height: "100%",
+              resizeMode: Constants.manifest.splash.resizeMode || "contain",
+              transform: [
+                {
+                  scale: animation,
+                },
+              ],
+            }}
+            source={image}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+function MainScreen() {
+  return (
+    <ScrollView style={{ backgroundColor: "#000033" }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 30,
+            marginBottom: 50,
+            fontWeight: "bold",
+            top: 30,
+          }}
+        >
+          Attendance
+        </Text>
+      </View>
+      <Date />
+      <Attendance />
+      <Chart />
+      <Stats />
+    </ScrollView>
+  );
+}
